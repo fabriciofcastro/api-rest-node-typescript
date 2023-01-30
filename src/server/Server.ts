9bf9ef75a29534a3071c1a81c6766e7199e0a177
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2023-01-30 13:47:39 -0300

    feat: adicionado ESLINT e EditorConfig
    Signed-off-by: Fabricio Castro <fabriciofeitosacastro@gmail.com>

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,11 +1,12 @@
-import express  from "express";
-const server = express();
+import express from 'express'
+const server = express()
+
 
 
 server.get('/', (req, res) => {
-  return res.send("Olá Dev!");
+	return res.send('Olá Dev!')
 })
-
+   
 
 
 export { server }
